6978502b4a44eb8456d3b903b7e0c4d4635a8fe2
felipemamore <felipemamore@gmail.com>
2022-10-17 20:24:10 -0300

    comitando prova

---
+++ prova02/src/ifsc/Caminhao.java
@@ -0,0 +1,34 @@
+package ifsc;
+
+public class Caminhao extends Veiculo{
+	private Integer eixos;
+	
+	public Caminhao() {
+		
+	}
+	
+	public Caminhao(String placa, Integer ano, Integer eixos) {
+		super.setPlaca(placa);
+		super.setAno(ano);
+		this.eixos=eixos;
+	}
+
+	public Integer getEixos() {
+		return eixos;
+	}
+
+	public void setEixos(Integer eixos) {
+		this.eixos = eixos;
+	}
+	
+	@Override
+	public String toString() {
+		
+		return "placa\n"+super.getPlaca()+"\n"+
+				"ano\n"+super.getAno()+"\n"+
+				"eixos\n"+this.eixos+"\n";
+	}
+	
+
+	
+}
---
+++ prova02/src/ifsc/Veiculo.java
@@ -0,0 +1,29 @@
+package ifsc;
+
+public abstract class Veiculo {
+	private String placa;
+	private Integer ano;
+	
+	
+
+	public String getPlaca() {
+		return placa;
+	}
+
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+
+	public Integer getAno() {
+		return ano;
+	}
+
+	public void setAno(Integer ano) {
+		this.ano = ano;
+	}
+	
+	
+
+}
+
+
